b63a6641f3c20a3fd374b3b3cfb9cc97b9d60560
mkfdev <rosy.fdev@gmail.com>
2021-08-15 17:02:17 +0900

    JSON data encoded text 처리

--- src/components/videoItem.jsx
+++ src/components/videoItem.jsx
@@ -1,15 +1,20 @@
 import React, { memo } from "react";
-import styles from "../styles/videoItem.module.css"
+import styles from "../styles/videoItem.module.css";
+import decode from "unescape";
 
 const VideoItem = memo(({ video, display, handleSelectVideo }) => {
     const displayType = (display === 'row') ? styles.row : styles.column;
+
+    const decodeTitle = title => {
+      return decode(title);
+    };
     return (
       <li className={`${styles.item} ${displayType}`} onClick={() => handleSelectVideo(video)}>
         <div className={styles.video}>
           <img className={styles.thumbnails} src={video.snippet.thumbnails.medium.url} alt="video thumbnail"/>
           <div className={styles.metadata}>
-            <p className={styles.title}>{video.snippet.title}</p>
-            <p className={styles.channelTitle}>{video.snippet.channelTitle}</p>
+            <p className={styles.title}>{decodeTitle(video.snippet.title)}</p>
+            <p className={styles.channelTitle}>{decodeTitle(video.snippet.channelTitle)}</p>
           </div>
         </div>
       </li>
